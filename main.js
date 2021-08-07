142f494135563d8dc4f5b921c45fa1bd8f2232d7
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-08-07 18:01:20 -0400

    Correct code for fuctionality

--- main.js
+++ main.js
@@ -9,39 +9,53 @@ function cleanAndCap (str){
 }
 
 // Assigning the variables with values used in the story
-const firstAnimal= cleanAndCap(words.get('animal-1'));
-const secondAnimal = cleanAndCap(words.get('animal-2'));
 
+//Nouns
+const noun1 = cleanAndCap(words.get('noun-1'));
+const noun2 = cleanAndCap(words.get('noun-2'));
+const noun3 = cleanAndCap(words.get('noun-3'));
+const noun4 = cleanAndCap(words.get('noun-4'));
+const noun5 = cleanAndCap(words.get('noun-5'));
+
+
+//Adjectives
+const adj1 = cleanAndCap(words.get('adj-1'));
+const adj2 = cleanAndCap(words.get('adj-2'));
+const adj3 = cleanAndCap(words.get('adj-3'));
+
+//Adverb
+const adv1 = cleanAndCap(words.get('adv-1'));
+
+//Verbs
+const verb1 = cleanAndCap(words.get('verb-1'));
+const verb2 = cleanAndCap(words.get('verb-2'));
+const verb3 = cleanAndCap(words.get('verb-3'));
+
+//Other
 const answer = cleanAndCap(words.get('answer'));
-const conjunction = answer === 'Yes' ? 'and' : 'but';
+const num1 = cleanAndCap(words.get('num-1'));
+const quote = cleanAndCap(words.get('quote'));
+const speed = cleanAndCap(words.get('speed'));
 
-const speed = words.get('speed');
-const adj1 = words.get('adj-1');
 
-const thirdAnimal = cleanAndCap(words.get('animal-3'));
-const quote = words.get('quote');
-
-const verb1 = words.get('verb-1');
-const num1 = words.get('num-1');
-const message = words.get('message');
 
 // The string containing HTML and text which will populate the story.html page
-const story = `<p>A <span class="word" title="id: animal-1">${firstAnimal}</span> was making fun of the <span class="word" title="id: animal-2">${secondAnimal}</span> one day for being so slow.</p>
+const story = `<p>There have been many attempts at creating a digital currency during the 90s tech boom, with systems like Flooz, <span class="word" title="id: noun-1">${noun1}</span> and DigiCash emerging on the market but inevitably failing. There were many different reasons for their failures, such as <span class="word" title="id: verb-1">${verb1}</span>, financial problems and even frictions between employees and their <span class="word" title="id: noun-2">${noun2}</span>.</p>
 
-<p>"Do you ever get anywhere?" he asked with a mocking laugh.</p>
+<p>Notably, all of those systems utilized a Trusted Third Party approach, meaning that the companies behind them <span class="word" title="id: verb-2">${verb2}</span> and <span class="word" title="id: verb-3">${verb3}</span> the transactions. Due to the failures of these companies, the creation of a digital cash system was seen as a <span class="word" title="id: adj-1">${adj1}</span> <span class="word" title="id: noun-3">${noun3}</span> for a long while.</p>
 
-<p>"<span class="word" title="id: answer">${answer}</span>," replied the <span class="word" title="id: animal-2">${secondAnimal}</span>, "${conjunction} I get there <span class="word" title="id: speed">${speed}</span> than you think. I'll run you a race and prove it."</p>
+<p>Then, in early 2009, a <span class="word" title="id: adj-2">${adj2}</span> programmer or a group of programmers under an alias Satoshi Nakamoto introduced Bitcoin. Satoshi described it as a <span class="word" title="id: adv-1">${adv1}</span> <span class="word" title="id: adj-3">${adj3}</span> cash system. It is completely decentralized, meaning there are no <span class="word" title="id: noun-4">${noun4}</span> involved and no central controlling authority. The concept closely resembles peer-to-peer networks for <span class="word" title="id: noun-5">${noun5}</span> sharing.</p>
 
-<p>The <span class="word" title="id: animal-1">${firstAnimal}</span> was much <span class="word" title="id: adj-1">${adj1}</span> at the idea of running a race with the <span class="word" title="id: animal-2">${secondAnimal}</span>, but for the fun of the thing he agreed. So the <span class="word" title="id: animal-3">${thirdAnimal}</span>, who had consented to act as judge, marked the distance yelled, "<span class="word" title="id: quote">${quote}</span>".</p>
+<p>Are you currently investing in cryptocurrency? <span class="word" title="id: answer">${answer}!</span></p>
 
-<p>The <span class="word" title="id: animal-1">${firstAnimal}</span> was soon far out of sight, and to make the <span class="word" title="id: animal-2">${secondAnimal}</span> feel very deeply how ridiculous it was for him to try a race with a <span class="word" title="id: animal-1">${firstAnimal}</span>, he went off the course to practice <span class="word" title="id: verb-1">${verb1}</span> for <span class="word" title="id: num-1">${num1}</span> hours until the <span class="word" title="id: animal-2">${secondAnimal}</span> should catch up.</p>
+<p>Prediction: In a few years you will own <span class="word" title="id: num-1">${num1}</span> shares of cryptocurrency. You might accumulate them at a <span class="word" title="id: speed">${speed}</span> than your peers.</p>`
+;
 
-<p>The <span class="word" title="id: animal-2">${secondAnimal}</span> meanwhile kept going slowly but steadily, and, after a time, passed the place where the <span class="word" title="animal-1">${firstAnimal}</span> was <span class="word" title="id: verb-1">${verb1}</span>. The <span class="word" title="id: animal-1">${firstAnimal}</span> was so caught up in <span class="word" title="id: verb-1">${verb1}</span>; and when at last he did stop, the <span class="word" title="id: animal-2">${secondAnimal}</span> was near the goal. The <span class="word" title="id: animal-1">${firstAnimal}</span> now ran his swiftest, but he could not overtake the <span class="word" title="id: animal-2">${secondAnimal}</span> in time.</p>`;
 
 // Grabbing the title element
 const title = document.getElementById('title');
 // Populating the title element with text
-title.innerHTML = `The <span class="word" title="id: animal-1">${firstAnimal}</span> And The  <span class="word" title="id: animal-2">${secondAnimal}</span>`;
+title.innerHTML = `The History of Cryptocurrency</span>`;
 
 // Grabbing the story element
 const storyEl = document.getElementById('story');
@@ -51,4 +65,4 @@ storyEl.innerHTML = story;
 // Grabbing the moral-message element
 const moralMessage = document.getElementById('moral-message');
 // Populating the moral-message element with text
-moralMessage.innerHTML = `<span class="italics" title="id: message">"${message}"</span>`;
+moralMessage.innerHTML = `<p>Just Remember: <span class="word" title="id: quote">${quote}</span></p>`;
